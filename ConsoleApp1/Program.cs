ed18b3a97a7419ec43454870680fe00b46853ecb
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:44:23 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string GetPluralized(string word, int count)
+            {
+                return count != 1 ? word + "s" : word;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                string dayOrDays = GetPluralized("day", i);
+                string msg = String.Format("{0} {1}", i, dayOrDays);
+                Write.Line(msg);
+            }
+        }
+    }
+}
